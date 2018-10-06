e2ce43af8e6b3cc3269df9dc9b9d71b77bc2bb1e
Dawid Mariankowski <dawid24.9@o2.pl>
2018-10-06 23:11:31 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+    {
+    const clasesAccordion = document.querySelectorAll('.gs-faq__question');
+    for(let = i; i<clasesAccordion.length; i++){
+document.querySelectorAll("[id^="tab"]").addEventListener("click", function(event) {
+}
+
+    for(let i =0; i<clasesAccordion.length; i++){
+        if(clasesAccordion[i].style.display == 'block'){
+            continue;
+        }else{
+document.clasesAccordion[i].style.display = 'none';
+        }
+
+
+}           
+event.preventDefault();
+}, false);
+    }
